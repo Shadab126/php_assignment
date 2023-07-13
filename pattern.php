1165cdba221243e5e026adc82d801f108e5e5c0b
Ansari Shadab <ansari.shadab@ratnaglobaltech.com>
2023-07-13 09:21:59 +0530

    Pattern Assignment

---
+++ pattern.php
@@ -0,0 +1,24 @@
+<?php  
+//Star Pattern
+for($i=1; $i<=5; $i++){   
+for($j=1; $j<=$i; $j++){   
+echo ' * ';   
+}  
+echo '<br>';   
+}  
+for($k=5; $k>=1; $k--){   
+for($l=1; $l<=$k; $l++){  
+echo ' * ';   
+}   
+echo '<br>';   
+}   
+
+//Number Pattern
+$maxLength = 4;
+for($i=1; $i <=10 ; $i++) { 
+    for($j=1; $j<=10;$j++){
+        
+     echo $i*$j." ";
+    }
+    echo "<br>";
+}
